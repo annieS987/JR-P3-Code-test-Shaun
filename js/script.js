6a4de5db26d1d671be616e98f66f348730aa0690
Lizhuo Sun <u7213426@anu.edu.au>
2022-08-29 14:49:47 +1000

    finished code test

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 // write your code here
+// check if the current number is a multiple of 3 or 5 or both
+var printNumbers = function(){
+    if(i%3 == 0 && i%5 == 0){
+        return "FizzBuzz";
+    }else if(i%3 == 0){
+        return "Fizz";
+    }else if(i%5 == 0){
+        return "Buzz";
+    }else{
+        return i;
+    }
+};
 
+for (var i=1; i<=100; i++){
+    document.write(printNumbers())
+    document.write("<br>");
+}
 
